f7f84878c54647335b77795f62f80161accee0f1
Matt Lewis <matt@mplewis.com>
2016-12-14 22:54:56 -0700

    test: route/search

--- test/test_routes.py
+++ test/test_routes.py
@@ -9,3 +9,15 @@ app = TApp(app_under_test)
 
 def test_root():
     app.get('/', status=204)
+
+
+def test_routes():
+    resp = app.get('/routes')
+    len(resp.json).should.equal(5)
+
+
+def test_route_search():
+    resp = app.get('/routes?q=2')
+    len(resp.json).should.equal(1)
+    name = resp.json[0]['route_long_name']
+    name.should.equal('Bullfrog - Furnace Creek Resort')
